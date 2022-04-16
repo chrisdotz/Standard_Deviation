0cd49fd11497d65ac8c6965c3d19ad6b939ef81d
chrisdotz <96923618+chrisdotz@users.noreply.github.com>
2022-04-15 18:42:13 -0700

    Create standard_deviation.py

---
+++ standard_deviation.py
@@ -0,0 +1,23 @@
+#only an example of standard deviation in python
+
+players = [180,172,178,185,190,195,192,200,210,190]
+add_player = 180 + 172 + 178 + 185+ 190 + 195 + 192 + 200 + 210 + 190
+mean_players = add_player / len(players)
+
+#x = (190 - mean_players)
+q=9.2 ** 2
+w=17.2 ** 2
+e=11.2 ** 2
+r=4.2 ** 2
+t=.80 ** 2
+y =5.8 ** 2
+u =2.8 ** 2
+i =10.8 ** 2
+o =20.8 ** 2
+p =.80 ** 2
+
+close = q+w+e+r+t+y+u+i+o+p
+close = close/9
+squareroot = close**.5
+
+print(squareroot)
